62bd0f7b9bb59fa932d1987a315c502871ba1c4a
carlos-username <carlosckg@gmail.com>
2015-02-15 23:01:29 -0600

    cosa

--- grayscale.py
+++ grayscale.py
@@ -5,21 +5,34 @@ from sys import argv
 import matplotlib.pyplot as plt
 #import numpy as np
 import matplotlib.cm as cm
-
-# img=rgb_scale(argv[1])
+img=rgb_scale(argv[1])
 ancho=len(img)
 alto=len(img[0])
-img2=[[0]*alto for j in xrange(ancho)]
+
 def luminosity_cof(r,g,b): #luminosity coefficients
     return r*0.2126 + 0.7152*g + 0.0722*b
 
 def suma(rgb):
     return sum(rgb)
-
-for i in xrange(ancho):
-    for j in xrange(alto):
-        img2[i][j]=luminosity_cof(img[i][j][0],img[i][j][1],img[i][j][2])
+def to_grayscale(img):
+    img2=[[0]*alto for j in xrange(ancho)]
+    for i in xrange(ancho):
+        for j in xrange(alto):
+            img2[i][j]=luminosity_cof(img[i][j][0],img[i][j][1],img[i][j][2])
+    return img2
         #img2[i][j]=suma(img[i][j])
+gray=to_grayscale(img)
 
-plt.imshow(img2,cmap = cm.Greys_r) #Drawing modified image with detected edges
+plt.imshow(gray,cmap = cm.Greys_r)
+plt.figure()
+def binarizacion(imagen):
+    for i in xrange(ancho):
+        for j in xrange(alto):
+            if imagen[i][j]<100:
+                imagen[i][j]=0
+            else:
+                imagen[i][j]=255
+    return imagen
+binar=binarizacion(gray)
+plt.imshow(binar,cmap = cm.Greys_r) #Drawing modified image with detected edges
 plt.show()
