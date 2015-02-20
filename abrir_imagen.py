f17f870b0023febe8fc1f4410ffbd569a818a6dd
carlos-username <carlosckg@gmail.com>
2015-02-20 05:08:00 -0600

    nuevo

--- abrir_imagen.py
+++ abrir_imagen.py
@@ -6,8 +6,8 @@ import cv2
 from grayscale import *
 def gray_scale(Img):
     image2 = cv2.imread(Img)
-    #image=cv2.resize(image2,(200,200))
-    gray_image = to_grayscale(image2)
+    image=cv2.resize(image2,(200,200))
+    gray_image = to_grayscale(image)
     return gray_image
 
 def rgb_scale(Img):
