df7144ab5149ebeb3e02d2cbc5d6fc7112c92606
carlos-username <carlosckg@gmail.com>
2015-03-17 04:03:10 -0600

    Circles

--- detectar_cuadro.py
+++ detectar_cuadro.py
@@ -3,7 +3,7 @@ import math
 import cv2
 def caja_envolvente(figura,imagen):
     puntos={"Xmax":(0,0),"Xmin":(0,0),"Ymax":(0,0),"Ymin":(0,0)}
-    puntos["Xmax"]=max(figura,key=lambda item:item[0]) # getting maxY,maxX,minX,minY
+    puntos["Xmax"]=max(figura,key=lambda item:item[0])
     puntos["Xmin"]=min(figura,key=lambda item:item[0])
     puntos["Ymax"]=max(figura,key=lambda item:item[1])
     puntos["Ymin"]=min(figura,key=lambda item:item[1])
@@ -12,7 +12,7 @@ def caja_envolvente(figura,imagen):
     #for r in puntos:
     #    imagen[puntos[r][0]][puntos[r][1]]=(255,255,0)
         
-    for q in xrange(puntos["Xmin"][0],puntos["Xmax"][0]): # drawing the bounding box
+    for q in xrange(puntos["Xmin"][0],puntos["Xmax"][0]):
         imagen[q][puntos["Ymin"][1]]=(51,255,51)
         
     for q in xrange(puntos["Ymin"][1],puntos["Ymax"][1]):
@@ -27,7 +27,7 @@ def caja_envolvente(figura,imagen):
     #distx=math.sqrt(pow(puntos["Xmax"][0]-puntos["Xmin"][0],2)+pow(puntos["Xmax"][1]-puntos["Xmin"][1],2))/2
     #disty=math.sqrt(pow(puntos["Ymax"][1]-puntos["Ymin"][1],2)+pow(puntos["Ymax"][0]-puntos["Ymin"][0],2))/2
     #imagen[puntos["Xmin"][0]+distx][puntos["Ymin"][1]+disty]=(0,0,0)
-def centro_masa(imagen,figura): #getting mass center
+def centro_masa(imagen,figura):
     ancho_im=len(imagen)
     alto_im=len(imagen[0])
     num_pix=len(figura)
@@ -35,6 +35,7 @@ def centro_masa(imagen,figura): #getting mass center
     promx=sum(sumax)/len(figura)
     sumay=[i[1] for i in figura]
     promy=sum(sumay)/len(figura)
-    imagen[promx][promy]=(0,0,0) #drawing the mass center within the figure
+    imagen[promx][promy]=(0,0,0)
     porciento_figura=num_pix*100.0/(ancho_im*alto_im)
     print "Porciento: ",porciento_figura
+    return (promx,promy)
